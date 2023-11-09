916f22aa03ab2d0007a959bc8b2e82f57fe6ab10
pritam-kumar-3778 <pritamkumarpatel1212@gmail.com>
2023-11-10 01:55:54 +0530

    Add Men's kurta section
    Add only men's kurta in all section for understanding and designing. Later on respective section contain respective data using API integeration.

--- src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
+++ src/customer/components/HomeSectionCarosel/HomeSectionCarosel.jsx
@@ -1,34 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
+import { mens_kurta } from "../../../Data/mens_kurta";
 // import { Button } from "@mui/material";
 // import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
-const HomeSectionCarosel = () => {
+const HomeSectionCarosel = ({data}) => {
+    // const [activeIndex,setActiveIndex]=useState(0);
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+//   const slidePrev=()=>setActiveIndex(activeIndex-1);
+//   const slideNext=()=>setActiveIndex(activeIndex+1);
+
+//   const syncActiveIndex=({item})=>setActiveIndex(item);
+
+
+  const items = data.slice(0,20).map((item) => <HomeSectionCard product={item}/>);
   return (
     <div className="px-4 lg:px-8">
       <div className="relative p-5">
         <AliceCarousel
           items={items}
           disableDotsControls
-          infinite
           responsive={responsive}
+        //   onSlideChange={syncActiveIndex}
+        //   activeIndex={activeIndex}
         />
 
-        {/* <button variant="contained" className="z-50", bgcolor:"white" sx={{position:'absolute', top:"8rem", right:"0rem", transform:"translateX(50%) rotate(90deg)", bgcolor:white}} aria-label="next">
+          {/* { activeIndex !== items.length-5 && <button variant="contained" className="z-50", bg-white onClick={slideNext} sx={{position:'absolute', top:"8rem", right:"0rem", transform:"translateX(50%) rotate(90deg)", bgcolor:white}} aria-label="next">
             <KeyboardArrowLeftIcon sx={{transform:"Rotate(90deg)", color:"black"}}/>
-        </button> */}
+        </button> }  */}
 
-         {/* <button variant="contained" className="z-50", bgcolor:"white" sx={{position:'absolute', top:"8rem", left:"0rem", transform:"translateX(50%) rotate(-90deg)", bgcolor:white}} aria-label="next">
+          {/* {activeIndex !== 0 && <button onClick={slidePrev} variant="contained" className="z-50", bgcolor:"white" sx={{position:'absolute', top:"8rem", left:"0rem", transform:"translateX(50%) rotate(-90deg)", bgcolor:white}} aria-label="next">
             <KeyboardArrowLeftIcon sx={{transform:"Rotate(90deg)", color:"black"}}/>
-        </button>  */}
+          </button>  }  */}
       </div>
     </div>
   );
--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import MainCrosel from '../../components/HomeCarosel/MainCrosel'
 import HomeSectionCarosel from '../../components/HomeSectionCarosel/HomeSectionCarosel'
+import { mens_kurta } from '../../../Data/mens_kurta'
 
 const HomePage = () => {
   return (
@@ -8,11 +9,12 @@ const HomePage = () => {
       <MainCrosel/>
 
       <div className='space-y-1 py-5 flex flex-col justify-center'>
-        <HomeSectionCarosel/>
-        <HomeSectionCarosel/>
-        <HomeSectionCarosel/>
-        <HomeSectionCarosel/>
-        <HomeSectionCarosel/>
+        <HomeSectionCarosel data={mens_kurta}/>
+        <HomeSectionCarosel data={mens_kurta}/>
+        <HomeSectionCarosel data={mens_kurta}/>
+        <HomeSectionCarosel data={mens_kurta}/>
+        <HomeSectionCarosel data={mens_kurta}/>
+        <HomeSectionCarosel data={mens_kurta}/>
       </div>
     </div>
   )
