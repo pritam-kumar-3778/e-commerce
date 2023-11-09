c17be7df840455aa2581cebfdf821bab77ec3273
pritam-kumar-3778 <pritamkumarpatel1212@gmail.com>
2023-11-09 22:18:58 +0530

    Add Navigation
    Add Navigation section of the project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      Hello E-Commerce
+    <div className="">
+      <Navigation/>
+      <div>
+        home
+      </div>
     </div>
   );
 }
