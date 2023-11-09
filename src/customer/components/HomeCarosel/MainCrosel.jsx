38d5c10e017df946d9fdb6cd515e1ba1f54c28b2
pritam-kumar-3778 <pritamkumarpatel1212@gmail.com>
2023-11-09 23:42:05 +0530

    Build Carousels
    Add 5 image as image slider.

--- src/customer/components/HomeCarosel/MainCrosel.jsx
+++ src/customer/components/HomeCarosel/MainCrosel.jsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
+import React from 'react'
+import { mainCarouselData } from './MainCaroselData'
+import AliceCarousel from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css';
 
+const MainCrosel = () => {
 
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="3">3</div>,
-    <div className="item" data-value="4">4</div>,
-    <div className="item" data-value="5">5</div>,
-];
-
-const MainCrosel = () => (
+    const items = mainCarouselData.map((item)=> <img className='cursor-pointer' 
+    role='presentation' src={item.image} alt=""/>)
+  return (
     <AliceCarousel
-        mouseTracking
         items={items}
-        controlsStrategy="alternate"
     />
-);
+  )
+}
 
-export default MainCrosel;
+export default MainCrosel
