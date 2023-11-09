535d0d9dea71f802dd5d02633d8665a3ed94a7ed
pritam-kumar-3778 <pritamkumarpatel1212@gmail.com>
2023-11-10 02:43:37 +0530

    Add something to footer
    Add something to footer.

--- src/customer/components/Footer/Footer.jsx
+++ src/customer/components/Footer/Footer.jsx
@@ -1,89 +1,130 @@
-import React from 'react'
-import { Button, Grid, Typography } from '@mui/material'
+import React from "react";
+import { Button, Grid, Link, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
     <div>
-      <Grid className='bg-black text-white text-center mt-10'
-      container
-      sx={{bgcolor:"black",color:"white",py:3}}
+      <Grid
+        className="bg-black text-white text-center mt-10"
+        container
+        sx={{ bgcolor: "black", color: "white", py: 3 }}
       >
+        <Grid item sx={12} sm={6} md={3}>
+          <Typography className="pb-5" variant="h6">
+            Company
+          </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              About
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Blog
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Press
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Jobs
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Partner
+            </Button>
+          </div>
+        </Grid>
 
         <Grid item sx={12} sm={6} md={3}>
+          <Typography className="pb-5" variant="h6">
+            Solutions
+          </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Marketing
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Analytics
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Commerce
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Insights
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Support
+            </Button>
+          </div>
+        </Grid>
 
-        <Typography className='pb-5' variant='h6'>Company</Typography>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>About</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Blog</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Press</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Jobs</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Partner</Button>
-        </div>
-        
+        <Grid item sx={12} sm={6} md={3}>
+          <Typography className="pb-5" variant="h6">
+            Documentation
+          </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Guides
+            </Button>
+          </div>
+        </Grid>
+
+        <Grid item sx={12} sm={6} md={3}>
+          <Typography className="pb-5" variant="h6">
+            Legal
+          </Typography>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Claim
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Privacy
+            </Button>
+          </div>
+          <div>
+            <Button className="pb-5" variant="h6" gutterBottom>
+              Terms
+            </Button>
+          </div>
         </Grid>
 
 
-
-        <Grid item sx={12} sm={6} md={3}>
-
-        <Typography className='pb-5' variant='h6'>Solutions</Typography>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Marketing</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Analytics</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Commerce</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Insights</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Support</Button>
-        </div>
-        
-        </Grid>
-
-
-
-        <Grid item sx={12} sm={6} md={3}>
-
-        <Typography className='pb-5' variant='h6'>Documentation</Typography>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Guides</Button>
-        </div>
-        
-        </Grid>
-
-
-
-        <Grid item sx={12} sm={6} md={3}>
-
-        <Typography className='pb-5' variant='h6'>Legal</Typography>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Claim</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Privacy</Button>
-        </div>
-        <div>
-        <Button className='pb-5' variant='h6' gutterBottom>Terms</Button>
-        </div>
-        
-        </Grid>
-
+          <Grid className='pt-20' item xs={12} >
+        <Typography variant="body2" component="p" align="center">
+          &copy; 2023 My Company. All rights reserved.
+        </Typography>
+        <Typography variant="body2" component="p" align="center">
+          Made with love by Me.
+        </Typography>
+        <Typography variant="body2" component="p" align="center">
+          {/* Icons made by{' '}
+          <Link href="https://www.freepik.com" color="inherit" underline="always">
+            Freepik
+          </Link>{' '}
+          from{' '}
+          <Link href="https://www.flaticon.com/" color="inherit" underline="always">
+            www.flaticon.com
+          </Link> */}
+        </Typography>
+      </Grid>  
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
