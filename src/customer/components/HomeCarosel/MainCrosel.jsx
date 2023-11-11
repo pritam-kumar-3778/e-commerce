34df0b2a0d0d5308f9b297d71ec1513474251596
pritam-kumar-3778 <pritamkumarpatel1212@gmail.com>
2023-11-11 23:02:58 +0530

    Continue working  of Navbar
    I am facing an error related to the no files matching and Module build failed. I still continuing my work to solve this error. This is my working progress of today.

--- src/customer/components/HomeCarosel/MainCrosel.jsx
+++ src/customer/components/HomeCarosel/MainCrosel.jsx
@@ -5,7 +5,7 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 
 const MainCrosel = () => {
 
-    const items = mainCarouselData.map((item)=> <img className='cursor-pointer' 
+    const items = mainCarouselData.map((item)=> <img className='cursor-pointer -z-10' 
     role='presentation' src={item.image} alt=""/>)
   return (
     <AliceCarousel
