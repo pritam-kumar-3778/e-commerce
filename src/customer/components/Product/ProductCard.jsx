b9fecc2d5c856b35a2e957dbe3925b35ee7a8ecf
pritam-kumar-3778 <pritamkumarpatel1212@gmail.com>
2023-11-14 10:34:35 +0530

    Start implementation of product section. Manually stored data.
    Now make only men's_kurta section.

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import "./productcard.css"
+import React from "react";
+import "./productcard.css";
 
-const ProductCard = () => {
+const ProductCard = ({product}) => {
   return (
-    <div className='productCard w-[15rem] m-3 transition-all cursor-pointer'>
-      <div className='h-[20rem] '>
-        <img className='h-full w-full object-cover object-left-top' src="https://rukminim1.flixcart.com/image/612/612/kflftzk0/top/q/t/m/l-aw-2081w-aelomart-original-imafwyzhw8t2npsk.jpeg?q=70" alt="" />
+    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
+      <div className="h-[20rem] ">
+        <img
+          className="h-full w-full object-cover object-left-top"
+          src={product.imageUrl}
+          alt=""
+        />
       </div>
-      <div className='textPart bg-white p-3'>
+      <div className="textPart bg-white p-3">
         <div>
-            <p className='font-bold opacity-60'>Brand name</p>
-            <p>Solid women white top.</p>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>{product.title}</p>
         </div>
-        <div className='flex items-center space-x-2 '>
-            <p className='font-semibold '>$299</p>
-            <p className='line-through opacity-60'>$599</p>
-            <p className='text-green-600 font-semibold'>40% off</p>
+        <div className="flex items-center space-x-2 ">
+          <p className="font-semibold ">₹{product.discountedPrice}</p>
+          <p className="line-through opacity-60">{product.price}</p>
+          <p className="text-green-600 font-semibold">{product.discountPersent}% off</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
